9d32f8f8062b269ab57016eb71f38eba83c20ba7
sulthanjihad <sulthanjihad444@gmail.com>
2021-09-30 18:13:42 +0700

    phpmvc
    setengah proses Assets

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-
-    <title>Halaman <?= $data['judul'];?></title>
+    <link rel="stylesheet" href="http://localhost/prakweb2021_phpmvc_193040048/public/css/bootstrap.css">
+    <title>Halaman <?= $data['judul'];?> </title>
 </head>
 <body>
