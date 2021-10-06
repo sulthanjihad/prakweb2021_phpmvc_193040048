386a9a326b32029033207c6de7ea5a1f151272ee
sulthanjihad <sulthanjihad444@gmail.com>
2021-10-07 01:52:01 +0700

    phpmvc
    model

---
+++ app/models/User_model.php
@@ -0,0 +1,12 @@
+<?php
+
+class User_model{
+    private $nama = 'Sulthan Jihad Abiyyu';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
+
+?>
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>About Me</h1>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/1.JPG" alt="Sulthan Jihad" width="200" class="rounded-circle shadow">
     <p>halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah <?= $data['pekerjaan'];?></p>
+    </div>
